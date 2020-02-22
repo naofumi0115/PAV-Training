4bc2d59459327fa11a1201563439ebe2af3401c5
Nguyen Thanh Bi <bint@pascaliaasia.com>
2020-02-22 21:43:36 +0700

    Init SOLID based on php code

---
+++ OpenClosedPrinciple/BadDesign/CostManager.php
@@ -0,0 +1,16 @@
+<?php
+
+class CostManager
+{
+    public function calculate($shape)
+    {
+        $costPerUnit = 1.5;
+        if ($shape instanceof Rectangle) {
+            $area = $shape->width * $shape->height;
+        } else {
+            $area = $shape->radius * $shape->radius * pi();
+        }
+        
+        return $costPerUnit * $area;
+    }
+}
